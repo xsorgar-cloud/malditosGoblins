8ba49de1ae6f51e6d5f526e973949620864661fa
xsorgar-cloud <xsorgar@gmail.com>
2026-06-05 14:47:27 +0200

    senda cazador

--- js/database.js
+++ js/database.js
@@ -158,7 +158,7 @@ const DB = {
     ],
     cazador: [
       { name: "· Camuflaje y Reflejos", desc: "Los Goblins tienen grandes reflejos y son difíciles de alcanzar. NO puedes asignar un dado de combate a una carta de ataque con valor de 1, 2 o 3." },
-      { name: "· Toxina Goblin", desc: "La senda está cubierta de una toxina nociva que no te deja respirar. Al final de tu turno (fase Post-Acción), recibes 1 Daño por cada Goblin vivo en la mesa." }
+      { name: "· Toxina Goblin", desc: "La senda está cubierta de una toxina nociva que no te deja respirar. Al final de tu turno recibes 1 Daño por cada Goblin vivo en la mesa." }
     ]
   },
 
