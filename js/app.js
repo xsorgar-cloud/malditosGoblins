38ae8234a8106ce2e08abae849c6af047ef36f47
xsorgar-cloud <xsorgar@gmail.com>
2026-09-14 02:15:28 +0200

    Tweak: Bajar el tope de tiempo de baile en combate a 2.5 segundos

--- js/app.js
+++ js/app.js
@@ -7043,7 +7043,7 @@ setInterval(() => {
             // Regla de combate: Bailan durante 2 a 3 segundos y luego se congelan para siempre
             if (!gob.combatTimerSet) {
                 gob.tauntState = 'dancing';
-                gob.tauntNextActionTime = now + 1500 + (Math.random() * 1500); // Entre 1500 y 3000 ms
+                gob.tauntNextActionTime = now + 1500 + (Math.random() * 1000); // Entre 1500 y 2500 ms
                 gob.combatTimerSet = true;
             }
             
